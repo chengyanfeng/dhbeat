cc8091918211df168939e5fb4e4d9b1b642cf87f
altman <sunhui@datahunter.cn>
2017-07-12 17:51:08 +0800

    init

--- main_test.go
+++ main_test.go
@@ -13,3 +13,9 @@ func TestProcFile(t *testing.T) {
 	i := ProcFile("/data/log/test.csv")
 	Debug(i)
 }
+
+func BenchmarkProcFile(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ProcFile("/data/log/test.csv")
+	}
+}
